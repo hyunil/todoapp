33dec1b819c02d20cd7d970c60795c71fec8b087
hyunil <pintory@gmail.com>
2017-03-14 20:02:43 +0000

    Delete action

--- todoapp/app/controllers/todos_controller.rb
+++ todoapp/app/controllers/todos_controller.rb
@@ -21,11 +21,7 @@ class TodosController < ApplicationController
     def edit
         @todo = Todo.find(params[:id])
     end
-    
-    def index
-        @todos = Todo.all
-    end
-    
+
     def update
         @todo = Todo.find(params[:id])
         if @todo.update(todo_params)
@@ -35,6 +31,18 @@ class TodosController < ApplicationController
             render 'edit'
         end
         
+    end
+
+    
+    def index
+        @todos = Todo.all
+    end
+
+    def destroy
+        @todo = Todo.find(params[:id])
+        @todo.destroy
+        flash[:notice] = "Todo was deleted succssfully"
+        redirect_to todos_path
     end
     
     private
